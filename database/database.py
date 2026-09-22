498fe4c4848b81a32182ec267ad4b1a935e94262
Krypton Yousuke <kryptonyousuke@proton.me>
2026-09-21 22:38:52 -0300

    db: enhance database management
    Signed-off-by: Krypton Yousuke <kryptonyousuke@proton.me>

--- database/database.py
+++ database/database.py
@@ -10,6 +10,11 @@ import aiosqlite
 class Database:
     def __init__(self, connection: aiosqlite.Connection):
         self.connection = connection
+    async def __aenter__(self):
+        return self
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        if self.connection:
+             await self.close()
 
     @classmethod
     async def create(cls, database_name):
@@ -17,9 +22,21 @@ class Database:
         return cls(connection)
 
     async def execute(self, query: str, parameters: tuple):
+        await self.connection.execute(query, parameters)
+        await self.connection.commit()
+
+
+    async def execute_and_fetch(self, query: str, parameters: tuple):
         async with self.connection.execute(query, parameters) as cursor:
             await self.connection.commit()
             return await cursor.fetchall()
+    
+    async def execute_and_fetch_one(self, query: str, parameters: tuple):
+        async with self.connection.execute(query, parameters) as cursor:
+            await self.connection.commit()
+            return await cursor.fetchone()
+    
+
 
     async def close(self):
         await self.cursor.close()
